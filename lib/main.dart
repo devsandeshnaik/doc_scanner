2a1b7a62c4a0a5abb43ea32e6911d440c4bc66b5
sandesh-naik <sandesh@indianrails.in>
2021-01-27 10:52:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:edge_detection/edge_detection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _imagePath = 'Unknown';
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text("scanner app"),
+        ),
+      ),
+    );
+  }
+}
